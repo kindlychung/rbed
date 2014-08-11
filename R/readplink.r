dcd53982ac7e8a87323de79b6853dd43a1fae400
Victor Chung <kindlychung@gmail.com>
2014-08-11 02:09:01 +0200

    doc

--- R/readplink.r
+++ R/readplink.r
@@ -1,24 +1,24 @@
 readplinkoutr = function(filename, colnameSelect=c("CHR", "SNP", "BP", "P"), covarHiden=FALSE) {
 	# allow globbing
 	filename = Sys.glob(filename)
-	
+
 	# extract colnames
     pilotColnames = read.table(filename, header=TRUE, nrows=1, stringsAsFactors=FALSE)
     cnames = colnames(pilotColnames)
-	
+
 	if(! covarHiden) {
 		# get index of selected cols
 		if(! all(colnameSelect %in% cnames)) {
 			stop("Some requested columns unavailable!")
 		}
 		colSelectIdx = which(cnames %in% colnameSelect)
-		
+
 		# calculate number of variables from the TEST col
 		testIdx = which(cnames == "TEST")
 		alltests = readcols(filename, testIdx, 1, 1)[[1]];
 		message("Take a look at the TEST column of the plink output file: ")
 		print(head(alltests))
-		
+
 		# assuming nobody would cram 500 variables into a GWAS model
 		if (length(alltests) > 500) {
 			ncovar = length(unique(alltests[1:500]))
@@ -50,4 +50,6 @@ readplinkoutr = function(filename, colnameSelect=c("CHR", "SNP", "BP", "P"), cov
 
     plinkout
 }
+
+
 
--- readme.md
+++ readme.md
@@ -1,3 +1,18 @@
+# Requirements
+
+* git
+* R packages:
+    * ggplot2
+    * Rcpp
+    * RcppArmadillo
+    * devtools
+
+# Installation
+
+    require(devtools)
+    Sys.setenv("PKG_CXXFLAGS"="-std=c++11")
+    install_github("rbed", username="kindlychung")
+
 # Changes
 
 * a minor fix for accomodating r command to changes in a c++ function
